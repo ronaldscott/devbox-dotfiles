1eaf540e66ce60ec2425a51fd00afc325b7acdde
Ronald <rscott@navexglobal.com>
2016-05-31 18:05:53 -0700

    add lemonbarr+barr as a status bar. bspwm tweaks. sxhkd tweaks.

---
+++ installer/install-barr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# install-barr.sh
+
+pacaur -Sya --needed --noconfirm --noedit --silent --noprogressbar ruby-barr
---
+++ installer/install-lemonbar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# install-lemonbar.sh
+
+pacaur -Sya --needed --noconfirm --noedit --silent --noprogressbar lemonbar-xft-git
