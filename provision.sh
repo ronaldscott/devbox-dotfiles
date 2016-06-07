1bbcd48444ce193d2bb4d7c3ae5287e223eb6323
Ronald Scott <ronaldscott@gmail.com>
2016-06-07 09:41:54 -0700

    add some finch-related files.

---
+++ provision.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# provision.sh
+
+pacaur -Sy --noconfirm --noedit python dmenu rxvt-unicode gtk-theme-numix-solarized tdrop-git lemonbar-xft-git google-chrome visual-studio-code glances ncdu xcursor-themes 
+xorg-xsetroot stalonetray fbpager-git
