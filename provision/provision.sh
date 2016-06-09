8cabf5be896ae09ed6b60f7944d55cd8c90a753d
Ronald Scott <ronaldscott@gmail.com>
2016-06-09 13:43:53 -0700

    add several packages to provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -4,3 +4,6 @@
 
 pacaur -Sy --noconfirm --noedit ttf-font-awesome xtitle yabar-git
 pacaur -Sy --noconfirm --noedit vte3-ng termite-git
+pacaur -Sy --noconfirm --noedit stalonetray
+pacaur -Sy --noconfirm --noedit glances
+pacaur -Sy --noconfirm --noedit ncdu
