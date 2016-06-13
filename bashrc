f65d853f3a5f58e47317e8a31f25ecb29885f313
Ronald Scott <ronaldscott@gmail.com>
2016-06-13 15:39:48 -0700

    add powerline for bash

--- bashrc
+++ bashrc
@@ -10,5 +10,12 @@ PS1='[\u@\h \W]\$ '
 
 # source everything in ~/.profile.d
 for f in ~/.profile.d/*.bash; do
-   . $f
+  . $f
 done
+
+if [ -f /usr/lib/python3.5/site-packages/powerline/bindings/bash/powerline.sh ]; then
+  powerline-daemon -q
+  POWERLINE_BASH_CONTINUATION=1
+  POWERLINE_BASH_SELECT=1
+  source /usr/lib/python3.5/site-packages/powerline/bindings/bash/powerline.sh
+fi
--- provision/provision.sh
+++ provision/provision.sh
@@ -7,5 +7,7 @@ pacaur -Sy --noconfirm --noedit vte3-ng termite-git
 pacaur -Sy --noconfirm --noedit stalonetray
 pacaur -Sy --noconfirm --noedit glances
 pacaur -Sy --noconfirm --noedit ncdu
+pacaur -Sy --noconfirm --noedit powerline
+pacaur -Sy --noconfirm --noedit ttf-roboto-mono-powerline-git
 
 curl -s -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.profile.d/git-completion.bash
