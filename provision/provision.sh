66ca43380db060dbd0a43e3e234c4134acc6140f
Ronald Scott <ronaldscott@gmail.com>
2016-06-15 10:12:45 -0700

    completion for docker, docker-compose, and npm

--- provision/provision.sh
+++ provision/provision.sh
@@ -3,3 +3,6 @@
 # provision.sh
 
 curl -s -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.profile.d/git-completion.bash
+curl -s -L https://raw.githubusercontent.com/docker/docker/master/contrib/completion/bash/docker >> ~/.profile.d/docker-completion.bash
+curl -s -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > ~/.profile.d/docker-compose-completion.bash
+npm completion >> ~/.profile.d/npm-completion.bash
