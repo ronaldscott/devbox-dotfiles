f833004774b10fbfb333e1b6a6ef44761c73936b
Ronald Scott <ronaldscott@gmail.com>
2016-06-14 23:22:10 -0700

    dotfiles should be distro-agnostic

--- provision/provision.sh
+++ provision/provision.sh
@@ -2,13 +2,4 @@
 
 # provision.sh
 
-pacaur -Sy --noconfirm --noedit ttf-font-awesome xtitle yabar-git
-pacaur -Sy --noconfirm --noedit vte3-ng termite-git
-pacaur -Sy --noconfirm --noedit stalonetray
-pacaur -Sy --noconfirm --noedit glances
-pacaur -Sy --noconfirm --noedit ncdu
-pacaur -Sy --noconfirm --noedit powerline
-pacaur -Sy --noconfirm --noedit ttf-roboto-mono-powerline-git
-pacaur -Sy --noconfirm --noedit gsimplecal-gtk2
-
 curl -s -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.profile.d/git-completion.bash
